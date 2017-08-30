6391347a113446ab2c200aebcfcd0afcfa340e6f
Chris Ross (ASP.NET) <chrross@microsoft.com>
2017-08-30 15:18:38 -0700

    Add WebHost as a HostedService sample

---
+++ samples/GenericWebHost/WebHostServiceOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace GenericWebHost
+{
+    public class WebHostServiceOptions
+    {
+        public Action<IApplicationBuilder> ConfigureApp { get; internal set; }
+    }
+}
